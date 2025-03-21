c896a77132c66c0d6e0e32cc4860b6316bc4e5b4
BarisTATAR <eng.baristatar@hotmail.com>
2025-03-21 21:59:02 +0300

    Company için putmapping ve postmapping işlemlerine başlandı.

---
+++ src/main/java/com/tys/request/UpdateCompanyRequest.java
@@ -0,0 +1,24 @@
+package com.tys.request;
+import com.tys.enums.CompanyCategory;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateCompanyRequest {
+
+    private Long id;
+    private String name;
+    private String address;
+    private CompanyCategory category;
+    private Integer startYear;
+    private String email;
+    private String phoneNumber;
+    private String taxNumber;
+    private String facilityNumber;
+    private Boolean electricChargeStation;
+    private Integer totalRoomNumber;
+
+}
